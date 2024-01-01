b397f06db384c5b1ee952fc1ec3dfa835bfd09ad
anthony morand <anthonymorand77@gmail.com>
2024-01-01 20:05:37 +0100

    upkg-path-plugins

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,40 @@
 import * as esbuild from 'esbuild-wasm';
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import ReactDOM from "react-dom";
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 
 const App = () => {
+    const ref = useRef<any>();
     const [input, setInput] = useState('');
     const [code, setCode] = useState('');
 
     const startService = async () => {
-        const service = await esbuild.startService({
+        ref.current = await esbuild.startService({
             worker: true,
             wasmURL: '/esbuild.wasm'
         });
-        console.log(service);
     };
 
     useEffect(() => {
         startService();
     }, [])
-    const onClick = () => {
-        console.log(input);
+
+    const onClick = async () => {
+        if (!ref.current) {
+            return;
+        }
+        
+        const result = await ref.current.build({
+            entryPoints: ['index.js'],
+            bundle: true,
+            write: false,
+            plugins: [unpkgPathPlugin()]
+        })
+        //console.log(result);
+        setCode(result.outputFiles[0].text);
     };
 
-    return <div>
+    return (<div>
         <textarea
             value={input}
             onChange={e => setInput(e.target.value)}
@@ -36,7 +49,7 @@ const App = () => {
             </button>
         </div>
         <pre>{code}</pre>
-    </div>;
+    </div>)
 };
 
 ReactDOM.render(
